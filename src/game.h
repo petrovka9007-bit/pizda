02b10a1f9eac015d415e68975601b43cababe75b
Petrovka9007 <petrovka9007@gmail.com>
2026-05-28 21:11:42 +0700

    Revert "Тут не я делал, а нейронка код написала, я понимаю как это работает, но сам бы хер написал"
    This reverts commit 5d9e01295fbad6f11d769399f348f1f538099ef6.

--- src/game.h
+++ src/game.h
@@ -9,22 +9,14 @@ namespace pizda {
 			camera.rotation = 0.f;
 			camera.offset = {0, 0};
 			camera.target = {0, 0};
-			hooiPosition = {screenWidth / 2.f, screenHeight / 2.f};
 		}
-
 		void processInput() {
-			if (IsKeyDown(KEY_RIGHT)) hooiPosition.x += hooiRotation;
-			if (IsKeyDown(KEY_LEFT))  hooiPosition.x -= hooiRotation;
-			if (IsKeyDown(KEY_DOWN))  hooiPosition.y += hooiRotation;
-			if (IsKeyDown(KEY_UP))    hooiPosition.y -= hooiRotation;
 		}
-
-		
 		void update(int) {
 		}
 		void draw() {
 			BeginMode2D(camera);
-			DrawTextureV(hooi, hooiPosition, WHITE);
+			DrawTexture(hooi, 0, 0, WHITE);
 			EndMode2D();
 		}
 		bool finished() {
@@ -34,7 +26,7 @@ namespace pizda {
 
 	private:
 		Camera2D camera;
-		
+
 		Texture loadTexture(const char* path) {
 			Texture result = LoadTexture(path);
 			GenTextureMipmaps(&result);
@@ -43,7 +35,5 @@ namespace pizda {
 		}
 
 		Texture hooi;
-		Vector2 hooiPosition;
-		float hooiRotation = 4.0f;
 	};
 }
